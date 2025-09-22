9cf54a3fe1998a7ddca5939e3afaaa359ad4f84f
marva kt <119792878+marvakt@users.noreply.github.com>
2025-09-22 21:04:16 +0530

    Time: 11 ms (32.76%), Space: 13.4 MB (81.29%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(key=lambda x:x==0)
+        
