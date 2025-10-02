d14bdae2a3046066fed7a9787fbc704eaad451d5
marva kt <119792878+marvakt@users.noreply.github.com>
2025-10-02 22:27:17 +0530

    Time: 3 ms (59.84%), Space: 12.5 MB (51.37%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        seen = set()  # to avoid infinite loops
+        seen = set()  
         while n != 1 and n not in seen:
             seen.add(n)
             n = sum(int(i)**2 for i in str(n))
