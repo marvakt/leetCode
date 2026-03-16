2f21f4eef7ee4960a78b49cc82eef2e91d8dc369
FATHIMA MARVA.KT <119792878+marvakt@users.noreply.github.com>
2026-03-16 12:57:33 +0530

    Time: 7 ms (16.26%), Space: 12.5 MB (89.22%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack=[]
+        pairs={']':'[',')':'(','}':'{'}
+        for i in s:
+            if i in pairs.values():
+                stack.append(i)
+            elif i in pairs:
+                if not stack or stack.pop()!=pairs[i]:
+                    return False
+        return not stack
